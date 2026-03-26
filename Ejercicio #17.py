d905cf1a29cee5cf465329d65f2ffedca22c1e2d
--add <oscardaniel@emilianisomacos.edu.gt>
2026-03-26 11:14:03 -0600

    Ejercicio 17 agregados

---
+++ Ejercicio #17.py
@@ -0,0 +1,23 @@
+print('----------------------------------------------')
+print('PROGREAMA: INVENTARIO PRODUCTO')
+print('Este programa te permite ver info de productos.')
+print('----------------------------------------------')
+
+nombre_producto = "Pantalla cell 100"
+codigo_producto = 1730
+precio = 700
+cantidad_stock = 29
+disponible = True
+
+print(nombre_producto)
+print(codigo_producto)
+print(precio)
+print(cantidad_stock)
+print(disponible)
+
+print("Marca: Cell")
+print("Modelo: 1000")
+print("Año: 2024")
+print("Color: gris ")
+print("Peso: 1.9 kg")
+print("Garantia: 3 años ")
